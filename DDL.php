dd20854b1620cf5d2e059bcd4ca41e137d7ed75a
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-27 16:31:58 +0900

    New: DDL

---
+++ DDL.php
@@ -0,0 +1,33 @@
+<?php
+/**	op-unit-sql-ci:/DDL.php
+ *
+ * @created    2025-11-27
+ * @version    1.0
+ * @package    op-unit-sql
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All rights reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Include sub directory files.
+$name = basename(__FILE__);
+$name = explode('.', $name)[0];
+foreach( glob(__DIR__."/{$name}/*.php") as $path ){
+	require_once($path);
+}
+
+//	...
+return $ci->Get();
